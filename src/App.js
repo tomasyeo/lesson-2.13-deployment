92b81d71903fe1a8de1fca474f9635dfb5948862
pigs-can-fly <edisen.zhuang@gmail.com>
2021-08-08 09:28:38 +0800

    test update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React (Deploy Test)
         </a>
       </header>
     </div>
